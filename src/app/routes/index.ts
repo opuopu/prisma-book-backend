5e6a263929c779f47742f299e50ed5a7cbafe161
opuopu <nurmdopu428@gmail.com>
2023-08-31 20:35:20 +0600

    first commit

---
+++ src/app/routes/index.ts
@@ -0,0 +1,16 @@
+// import express from 'express';
+
+// const router = express.Router();
+
+// const moduleRoutes = [
+//   // ... routes
+
+
+//   {
+//     path: '/offered-course-class-schedules',
+//     route: ""
+//   }
+// ];
+
+// moduleRoutes.forEach(route => router.use(route.path, route.route));
+// export default router;
