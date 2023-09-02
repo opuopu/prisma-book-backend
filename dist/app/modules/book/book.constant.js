a289b74dd825d15f2524807fa0af8516e13ef4a6
opuopu <nurmdopu428@gmail.com>
2023-09-02 15:47:15 +0600

    some

--- dist/app/modules/book/book.constant.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.booksSearchableFields = exports.booksfilterblefield = exports.booksfilterableOptions = void 0;
-exports.booksfilterableOptions = ["search", "minPrice", "maxPrice", "category"];
-exports.booksfilterblefield = ["price", "id"];
-exports.booksSearchableFields = ["title", "author", "genre"];
