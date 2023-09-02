d43b21bb135140f17e06ef1b4a8f016e4c1d7e81
opuopu <nurmdopu428@gmail.com>
2023-09-02 13:36:07 +0600

    done

--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -1,4 +1,4 @@
-import { WeekDays } from "@prisma/client";
+
 
 export const asyncForEach = async (array: any[], callback: any) => {
     if (!Array.isArray(array)) {
@@ -9,27 +9,27 @@ export const asyncForEach = async (array: any[], callback: any) => {
     }
 }
 
-export const hasTimeConflict = (
-    existingSlots: {
-        startTime: string,
-        endTime: string,
-        dayOfWeek: WeekDays
-    }[],
-    newSlot: {
-        startTime: string,
-        endTime: string,
-        dayOfWeek: WeekDays
-    }
-) => {
-    for (const slot of existingSlots) {
-        const existingStart = new Date(`1970-01-01T${slot.startTime}:00`);
-        const existingEnd = new Date(`1970-01-01T${slot.endTime}:00`);
-        const newStart = new Date(`1970-01-01T${newSlot.startTime}:00`);
-        const newEnd = new Date(`1970-01-01T${newSlot.endTime}:00`);
+// export const hasTimeConflict = (
+//     existingSlots: {
+//         startTime: string,
+//         endTime: string,
+//         dayOfWeek: WeekDays
+//     }[],
+//     newSlot: {
+//         startTime: string,
+//         endTime: string,
+//         dayOfWeek: WeekDays
+//     }
+// ) => {
+//     for (const slot of existingSlots) {
+//         const existingStart = new Date(`1970-01-01T${slot.startTime}:00`);
+//         const existingEnd = new Date(`1970-01-01T${slot.endTime}:00`);
+//         const newStart = new Date(`1970-01-01T${newSlot.startTime}:00`);
+//         const newEnd = new Date(`1970-01-01T${newSlot.endTime}:00`);
 
-        if (newStart < existingEnd && newEnd > existingStart) {
-            return true
-        }
-    }
-    return false;
-}
+//         if (newStart < existingEnd && newEnd > existingStart) {
+//             return true
+//         }
+//     }
+//     return false;
+// }
